73301e4117ed4ce49e6efee09e611c86a9019b12
Ray Li <k.r.li07717@gmail.com>
2022-07-01 13:16:29 +0800

    feat: auto like with db

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,29 +1,165 @@
-import config from "../config.json";
+import puppeteer from "puppeteer";
 import type { Page } from "puppeteer";
 import log from "./logger";
+import { BotUser, Like, Niche, dataSource } from "../db";
+import { DateTime } from "luxon";
+import type { IConfig, ITag } from "../types";
+import * as Config from "../config.json";
 
+const checkConfig = (config: IConfig) => config;
+const convertType = <T>(arg: any) => arg as T;
+
+const LIKES_LIMIT_OF_DAY = 20;
+const LIKES_LIMIT_PER_NICHE = 6;
+const LIKE_INTERVAL_MILLISECONDS = 2000; //todo: speed in ~120 likes/hour
 const INSTAGRAM_WEBSITE = "https://www.instagram.com/";
 const getTagUrl = (tag: string) =>
   `https://www.instagram.com/explore/tags/${tag}/`;
-const LIKES_LIMIT_OF_DAY = 5;
 
 export default class Bot {
-  readonly username: string = config.username;
-  readonly password: string = config.password;
-  readonly tagsToLike: string[] = config.tagsToLike;
-  likesLeaved = 0; /** should get from db */
+  readonly username: string = Config.username;
+  private readonly password: string = Config.password;
+  niches: Niche[];
+
+  likesLeavedToday: number;
+  currentNiche: Niche;
+  currentNicheLikes: number;
 
   private page: Page;
 
-  constructor(page: Page) {
-    this.page = page;
-  }
+  constructor() {}
 
   async init() {
     try {
       log("bot init");
-      await this.page.goto(INSTAGRAM_WEBSITE);
-      await this.login();
+      await this.initDb();
+      this.niches = this.niches.sort((a, b) => a.priority - b.priority);
+
+      // check likes today reach limit
+      await this.getLikesLeavedToday();
+      if (this.likesLeavedToday >= LIKES_LIMIT_OF_DAY)
+        throw new Error("Likes leaved today already reach limit");
+
+      // todo: check followers
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async prepareBrowserPage() {
+    try {
+      log("preparing browser page");
+      // init browser & page
+      const browser = await puppeteer.launch({ headless: false, slowMo: 150 });
+      this.page = await browser.newPage();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async initDb() {
+    try {
+      log("init db");
+      const config = checkConfig(Config);
+      const { username, tagsToLike } = config;
+      // await dataSource.initialize();
+
+      // check if botUser already exists
+      const dbBotUser = await dataSource.manager.findOneBy(BotUser, {
+        username,
+      });
+
+      if (!dbBotUser) {
+        if (!config.tagsToLike?.length)
+          throw new Error(
+            "new user must specify niche tags as string[] in config.json"
+          );
+        //todo: ensure tag name all diffrent string, priority all diffrent number
+
+        const tags = convertType<ITag[]>(tagsToLike);
+
+        // create niches
+        const niches = [];
+        for (let i = 0; i < tags.length; i++) {
+          const niche = new Niche();
+          niche.nameTag = tags[i].name;
+          niche.priority = tags[i].priority;
+          niche.likes = [];
+          niches.push(niche);
+          await dataSource.manager.save(niche);
+          log("create new niche", niche.nameTag);
+        }
+
+        // create botUser
+        const botUser = new BotUser();
+        botUser.username = username;
+        botUser.niche = niches;
+        await dataSource.manager.save(botUser);
+        log("created new bot user", username);
+        this.niches = niches;
+        return;
+      }
+
+      const niches = await dataSource
+        .getRepository(Niche)
+        .createQueryBuilder("niche")
+        .where("niche.botUserId = :id", { id: dbBotUser.id })
+        .getMany();
+
+      if (tagsToLike?.length) {
+        // todo: handle update tags
+      }
+
+      log("use db niches");
+      this.niches = niches;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async getCurrentNicheLikes() {
+    try {
+      const nicheId = this.currentNiche.id;
+      const todayStart = DateTime.now().toFormat("yyyy-LL-dd") + " 00:00:00";
+      const todayEnd = DateTime.now().toFormat("yyyy-LL-dd") + " 23:59:59";
+      const nicheLikes = await dataSource
+        .getRepository(Like)
+        .createQueryBuilder("like") /** ambiguous column name */
+        .select("__like")
+        .from(Like, "__like")
+        .where("__like.timestamp >= :todayStart", { todayStart })
+        .andWhere("__like.timestamp <= :todayEnd", { todayEnd })
+        .andWhere("__like.nicheId = :nid", { nid: nicheId })
+        .getMany();
+
+      this.currentNicheLikes = nicheLikes.length;
+      log(
+        `niche ${this.currentNiche.nameTag} likes today:`,
+        this.currentNicheLikes
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async getLikesLeavedToday() {
+    try {
+      const todayStart = DateTime.now().toFormat("yyyy-LL-dd") + " 00:00:00";
+      const todayEnd = DateTime.now().toFormat("yyyy-LL-dd") + " 23:59:59";
+      const nicheIds = this.niches.map((n) => n.id);
+
+      const allLikesToday = await dataSource
+        .getRepository(Like)
+        .createQueryBuilder("like")
+        .select("_like")
+        .from(Like, "_like")
+        .where("_like.timestamp >= :todayStart", { todayStart })
+        .andWhere("_like.timestamp <= :todayEnd", { todayEnd })
+        .andWhere("_like.nicheId IN (:...id)", { id: nicheIds })
+        .getMany();
+
+      this.likesLeavedToday = allLikesToday.length;
+      log("bot likes leaved today: ", this.likesLeavedToday);
     } catch (error) {
       throw error;
     }
@@ -32,16 +168,46 @@ export default class Bot {
   async likeRecentNichePosts() {
     try {
       log("like recent niche posts");
-      await this.findFirstRecentNichePost();
-      //todo: while < daily likes limit
-      //todo: speed in ~120 likes/hour
-      //todo: remember first like post time, so next time won't start from it again
-      while (this.likesLeaved < LIKES_LIMIT_OF_DAY) {
+      if (!this.niches || !this.page)
+        throw new Error("bot not yet prepared, try init() then login()");
+
+      for (let i = 0; i < this.niches.length; i++) {
+        if (this.likesLeavedToday >= LIKES_LIMIT_OF_DAY) break;
+        this.currentNiche = this.niches[i];
+        await this.getCurrentNicheLikes();
+        if (this.currentNicheLikes >= LIKES_LIMIT_PER_NICHE) continue;
+        await this.findFirstRecentNichePost();
+        await this.likeNiche();
+      }
+
+      if (this.likesLeavedToday >= LIKES_LIMIT_OF_DAY) {
+        log(`likes reached day limit ${LIKES_LIMIT_OF_DAY}`);
+      } else {
+        log("auto like process complete.");
+      }
+      process.exit(0);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async likeNiche() {
+    try {
+      log("liking niche: ", this.currentNiche.nameTag);
+      while (
+        this.likesLeavedToday < LIKES_LIMIT_OF_DAY &&
+        this.currentNicheLikes < LIKES_LIMIT_PER_NICHE
+      ) {
+        /**TODO: reach niche limit then getNextNiche */
         await this.likePost();
         await this.goToNextPost();
       }
-      log(`likes reached day limit ${LIKES_LIMIT_OF_DAY}`);
-      //handle if there's no more posts
+      if (this.currentNicheLikes >= LIKES_LIMIT_PER_NICHE) {
+        log(
+          `niche ${this.currentNiche.nameTag} reach day limit: `,
+          LIKES_LIMIT_PER_NICHE
+        );
+      }
     } catch (error) {
       throw error;
     }
@@ -53,19 +219,37 @@ export default class Bot {
       if (!shouldLikeThis) {
         return;
       }
-      const like = await this.page.$eval("*[aria-label=讚]", (node) =>
+
+      await this.page.$eval("*[aria-label=讚]", (node) =>
         node.parentElement?.parentElement?.click()
       ); // click like
 
-      const user = await this.page.$eval(
+      const userLiked = await this.page.$eval(
         "*[role=dialog] h2",
         (node) => node.textContent
       );
-      log(`like post from ${user}`); //store to db
+      log(`like post from ${userLiked}`);
 
-      this.likesLeaved++;
+      await this.storeLikeToDb(userLiked);
 
-      await this.page.waitForTimeout(5000);
+      this.likesLeavedToday++;
+      this.currentNicheLikes++;
+
+      await this.page.waitForTimeout(LIKE_INTERVAL_MILLISECONDS);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async storeLikeToDb(userLiked: string) {
+    try {
+      const like = new Like();
+      like.toUser = userLiked;
+      like.niche = this.currentNiche;
+      like.timestamp = DateTime.now().toFormat("yyyy-LL-dd HH:mm:ss");
+
+      await dataSource.manager.save(like);
+      log("stored a like");
     } catch (error) {
       throw error;
     }
@@ -74,6 +258,7 @@ export default class Bot {
   private async goToNextPost() {
     try {
       log("go to next post");
+      //todo: handle if no more post
       await this.page.$eval("*[aria-label=下一步]", (node) =>
         node.parentElement?.click()
       );
@@ -87,7 +272,7 @@ export default class Bot {
     try {
       await this.page.waitForSelector(
         "*[role=dialog] h2"
-      ); /** wait for first post user selector */
+      ); /** wait for first post username selector */
     } catch (error) {
       throw error;
     }
@@ -104,11 +289,18 @@ export default class Bot {
       if (liked) {
         return false;
       }
-      // check if user is already follower
+
+      // check if user is already follower ?
+      // get followers list
 
       // check if likes-of-post <= 200
+      /** handle hide likes */
+      /** handle yet no likes */
+      /** handle "萬" likes */
 
       // check if is already 2nd post of same user I've liked today
+
+      // check if is shadow banned user (already be giving likes yet still not follower)
 
       return true;
     } catch (error) {
@@ -118,17 +310,18 @@ export default class Bot {
 
   private async findFirstRecentNichePost() {
     try {
-      log("find first recent niche post");
+      log("find first recent post, niche:", this.currentNiche.nameTag);
       await this.page.goto(
-        getTagUrl(
-          this.tagsToLike[0]
-        ) /** niche tags should be hot or should be spreaded; should have a priority list */
+        getTagUrl(this.currentNiche.nameTag)
       ); /** open other pages and like? sound too Bot */
       await this.handleModalDialog();
       const firstPostLink = await this.page.waitForSelector(
         "h2:nth-child(2) + div *[role=link]"
       );
-      if (!firstPostLink) throw new Error("first post link not found on page");
+      if (!firstPostLink)
+        throw new Error(
+          "first post link not found on page"
+        ); /**TODO: go to next niche */
       await firstPostLink.click();
       await this.ensurePostModalIsReady();
     } catch (error) {
@@ -136,9 +329,28 @@ export default class Bot {
     }
   }
 
-  private async login() {
+  async login() {
     try {
       log("bot login");
+
+      if (!this.niches)
+        throw new Error("no niches found, try call init() first");
+
+      await this.prepareBrowserPage();
+
+      await this.page.goto(INSTAGRAM_WEBSITE);
+
+      /** if logged in */
+      // await this.page.waitForTimeout(3000);
+      // const accountImg = await this.page.$("nav img");
+      // if (accountImg) {
+      //   let regex = /([\w_]+)的大頭貼照/;
+      //   const account = regex.exec(
+      //     accountImg.getProperty("alt") as unknown as string
+      //   )?.[1];
+      //   log(`account ${account} already logged in`);
+      //   return;
+      // }
       const usernameInput = await this.page.waitForSelector(
         "input[name=username]"
       );
@@ -149,6 +361,7 @@ export default class Bot {
       await passwordInput?.type(this.password);
       await passwordInput?.press("Enter");
       await this.page.waitForNavigation();
+      /** handle 2 step auth */
     } catch (error) {
       throw error;
     }
---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export interface ITag {
+  name: string;
+  priority: number;
+}
+
+export interface IConfig {
+  username: string;
+  password: string;
+  tagsToLike?: ITag[];
+}
